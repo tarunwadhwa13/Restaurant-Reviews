680c3bf810d13a62087a2bc85706e7fd89f7bd9e
Tarun Wadhwa <Tarun.Wadhwa@go-mmt.com>
2018-04-12 15:51:12 +0530

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,43 @@
+const cacheName = "res";
+
+const assets = [
+    "/",
+    "/index.html",
+    "/restaurant.html",
+    "/css/styles.css",
+    "/img/1.jpg",
+    "/img/2.jpg",
+    "/img/3.jpg",
+    "/img/5.jpg",
+    "/img/5.jpg",
+    "/img/6.jpg",
+    "/img/7.jpg",
+    "/img/8.jpg",
+    "/img/9.jpg",
+    "/img/10.jpg",
+    "/js/main.js",
+    "/js/dbhelper.js",
+    "/js/restaurant_info.js",
+];
+
+
+// Installing resources for offline use 
+self.addEventListener('install', e => {
+    console.log("Installing assets")
+    e.waitUntil(
+        caches.open(cacheName)
+              .then(cache => cache.addAll(assets))
+              .catch(error => console.error("Error Occured", error))
+            );
+});
+
+// Match in cache and fetch
+self.addEventListener('fetch', function (event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+        })
+        );     
+});
+
